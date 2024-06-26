85db032b99982a16c360624be8120f0339a8c384
Kayemba Luwaga <76554604+KayembaLuwaga@users.noreply.github.com>
2024-06-26 08:58:35 +0300

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This will be your static file collection directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This will be your static file collection directory
 
 # Login url
 
